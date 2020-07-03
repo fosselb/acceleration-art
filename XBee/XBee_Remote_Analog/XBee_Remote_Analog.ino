28677b46fbbd3c5c5e99017726a47370448cf997
fosselb <flinbian@lion.lmu.edu>
2020-07-02 17:05:55 -0700

    added XBee_Remote_Analog.ino to test receiving analog data from remote XBee.

--- XBee/XBee_Remote_Analog/XBee_Remote_Analog.ino
+++ XBee/XBee_Remote_Analog/XBee_Remote_Analog.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+// XBee_Remote_Analog.ino
+// Author: Fosse Lin-Bianco
+// Purpose: To receive analog data from a remote XBee
+//    and print values to Serial Monitor. Data generated from
+//    XCTU software.
+// Hardware: 1) XBee + XBee Shield + Arduino RedBoard
+//           2) XBee + Dongle
 
+#include <SoftwareSerial.h>
+
+SoftwareSerial XBee(2, 3); // XBee DOUT, IN - Arduino pin 2, 3 (RX, TX
+
+void setup() {
+  XBee.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (XBee.available()) {
+    Serial.write(XBee.read());
+  }
 }
