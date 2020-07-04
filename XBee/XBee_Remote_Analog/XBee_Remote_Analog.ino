2754b02a1eeae6ba2c281550e7d526ac46ffecd8
fosselb <flinbian@lion.lmu.edu>
2020-07-03 19:03:57 -0700

    updated XBee_Remote_Analog.ino to test receiving analog data. Data is partially parsed.

--- XBee/XBee_Remote_Analog/XBee_Remote_Analog.ino
+++ XBee/XBee_Remote_Analog/XBee_Remote_Analog.ino
@@ -8,7 +8,10 @@
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial XBee(2, 3); // XBee DOUT, IN - Arduino pin 2, 3 (RX, TX
+char startByte = 0x7E;
+int dataCount = 0;
+
+SoftwareSerial XBee(2, 3); // XBee DOUT, IN - Arduino pin 2, 3 (RX, TX)
 
 void setup() {
   XBee.begin(9600);
@@ -16,7 +19,72 @@ void setup() {
 }
 
 void loop() {
+//  if (XBee.available()) {
+//    //Serial.write(XBee.read());
+////    Serial.print(XBee.read());
+//    
+////    Serial.print(XBee.read(), HEX);
+////    Serial.println();
+//  }
+
+//  if (XBee.available()) {
+//    if (XBee.read() == 0x7E) {
+//      for (int i = 0; i < 22; i++) {
+//      Serial.print(XBee.read(), HEX);
+//      Serial.print(",");
+//      }
+//    }
+//    Serial.println();
+//  }
+
+//  if (XBee.available()) {
+//    Serial.print("Serial.print():");
+//    Serial.print(XBee.read());
+//    Serial.println();
+//    Serial.print("Serial.print() with HEX:");
+//    Serial.print(XBee.read(), HEX);
+//    Serial.println();
+//  }
+
+//  char c = 0x7E;
+//  Serial.println(c, HEX);
+//  Serial.println(c == 0x7F);
+//  delay(1000);
+  
   if (XBee.available()) {
-    Serial.write(XBee.read());
+//    char current = XBee.read();
+//    if (current == 0x7E) {
+//      Serial.println();
+//    }
+//    
+//    for (int i = 0; i < 21; i++) {
+//      Serial.print(current, HEX);
+//      Serial.print(",");
+//    }
+
+    char current = XBee.read();
+    if (current == 0x7E) {
+      Serial.println();
+      Serial.println();
+      Serial.println("Start:");
+      dataCount = 0;
+    }
+    
+    
+    Serial.print(current, HEX);
+    Serial.print(",");
+    dataCount++;
+
+    if (dataCount == 8) {
+      Serial.print("*");
+      Serial.println();
+      byte analog_MSB = XBee.read();
+      byte analog_LSB = XBee.read();
+      int analog_reading = (analog_MSB * 256) + analog_LSB;
+      Serial.println(analog_reading);
+      dataCount = 0;
+    } else if (dataCount == 10) {
+      Serial.print("*");
+    }
   }
 }
