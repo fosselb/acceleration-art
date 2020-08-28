36b4b0087c8ac304cf8c3613756f34cbfa3d4bc5
fosselb <flinbian@lion.lmu.edu>
2020-08-28 08:41:30 -0700

    cleaning repository

--- arduino-processing-connection/Arduino/Hello_World/Hello_World.ino
+++
@@ -1,8 +0,0 @@
-void setup() {
-  Serial.begin(9600);               // initialize serial communication
-}
-
-void loop() {
-  Serial.println("Hello World!");   // print
-  delay(100);                       // wait 100 ms
-}
